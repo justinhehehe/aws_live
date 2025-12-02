e05152d80b3617383522b8f1b2a88cc5f6944ae7
Justin <justinwym-wm22@student.tarc.edu.my>
2025-12-02 21:52:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.cy8zqifceohq.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3273"
+custompass = "admin123"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "justin-employee"
 customregion = "us-east-1"
 
